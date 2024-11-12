6b4c7cc9b86b112965f8d7fc897fe7480a832350
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-12 17:19:07 +0100

    feat: add the ApplicationUser Model

--- lexicon-identity/Data/Migrations/20241112160652_Updated_User_Table.Designer.cs
+++ lexicon-identity/Data/Migrations/20241112160652_Updated_User_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using lexicon_identity.Data;
 
@@ -11,9 +12,11 @@ using lexicon_identity.Data;
 namespace lexicon_identity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241112160652_Updated_User_Table")]
+    partial class Updated_User_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ lexicon-identity/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace lexicon_identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
