351ef861199538f038e0548a295617dc82c3b942
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-12 17:38:34 +0100

    fix: change defaultIdentity to ApplicationUser and skaffold register and login pages again

--- lexicon-identity/Program.cs
+++ lexicon-identity/Program.cs
@@ -1,4 +1,5 @@
 using lexicon_identity.Data;
+using lexicon_identity.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,8 @@ namespace lexicon_identity
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //change declaration and skaffold identity pages again
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
