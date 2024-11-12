dcc464f9ec4db274804f7a3f82a36f360be20617
Svitlana Rybakova <rsv0011@gmail.com>
2024-11-12 19:58:27 +0100

    feat: limiting access based on role

---
+++ lexicon-identity/Controllers/IdentityController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lexicon_identity.Controllers
+{
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            return View();
+        }
+    }
+}
